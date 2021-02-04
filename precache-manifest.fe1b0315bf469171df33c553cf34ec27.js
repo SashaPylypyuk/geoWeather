9e51f577b8b9ec6923bb1935974b4e448819fc25
SahaPylypyuk <sashapulupyk@gmail.com>
2021-02-04 12:31:26 +0200

    Updates

---
+++ precache-manifest.fe1b0315bf469171df33c553cf34ec27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96e586b885505e2f9d649040cd1a7645",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e98dbf248451d27d67ba",
+    "url": "./static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "e98dbf248451d27d67ba",
+    "url": "./static/js/2.f6296dde.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "./static/js/2.f6296dde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7df7278e140e602748ba",
+    "url": "./static/js/main.4bd95e29.chunk.js"
+  },
+  {
+    "revision": "c00544cc02330c8c78a7",
+    "url": "./static/js/runtime-main.9a7464de.js"
+  }
+]);
